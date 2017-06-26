1d1b1ff85f09400392611e67a0b625cfa6914a64
malcolmh <malcolm.herring@btinternet.com>
2017-06-26 07:29:05 +0100

    update gpl notice

--- src/mndd.c
+++ src/mndd.c
@@ -6,12 +6,12 @@
  Description : Marine Network Data Decoder
  Copyright   : © 2016 Malcolm Herring.
 
- mnddd is free software: you can redistribute it and/or modify
+ mndd is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, version 3 of the License, or
  any later version.
 
- mnddd is distributed in the hope that it will be useful,
+ mndd is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
