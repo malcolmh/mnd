71abc5ff6ada72078428439b725810747c928805
Malcolm Herring <malcolm.herring@btinternet.com>
2025-03-12 07:48:51 +0000

    Tidy wavinit

--- src/wavinit.c
+++ src/wavinit.c
@@ -14,10 +14,10 @@
 void wav_init(int dev) {
 
     uint8_t init[] = { 0xaa, 0x55, 0x12, 0x05, 0x02, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0x01, 0, 0, 0, 0, 0x1a };
+        0, 0, 0, 0x01, 0, 0, 0, 0, 0x1a }; // Speed=250000, Extended Frame, Mode=Normal
     struct termios2 tio;
 
-    if (ioctl(dev, TCGETS2, &tio) == -1) {
+    if (ioctl(dev, TCGETS2, &tio) < 0) {
         perror("TTY tcgets2");
         exit(1);
     }
@@ -36,7 +36,7 @@ void wav_init(int dev) {
     }
 
     if (write(dev, init, sizeof(init)) < 0) {
-        perror("Write initr");
+        perror("Write init");
         exit(1);
     }
     return;
